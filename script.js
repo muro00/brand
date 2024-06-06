6b33c89f5509fe8d25de0901ca498a5308af7b0c
JeongYucheol <ask@left3steps.com>
2024-06-06 17:54:44 +0900

    003

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
     scrollHorizontally: true,
-    responsiveWidth: 768, // 768px 이하의 화면에서는 autoScrolling 비활성화
+    responsiveWidth: 320, // 768px 이하의 화면에서는 autoScrolling 비활성화
     responsiveHeight: 600,
     responsiveSlides: true
 });
